dd44e4a6cb7a466e36bf660fa6c5bec8ed61339c
Ruairi Dorrity <ruairidorrity@gmail.com>
2019-05-07 16:47:36 -0700

    Max dimension of 32 pixels. Added confirmation modal before clearing canvas, just in case.

--- src/components/Canvas/CanvasControls.js
+++ src/components/Canvas/CanvasControls.js
@@ -5,6 +5,7 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Modal from 'react-bootstrap/Modal';
 
 type Props = {
     initialWidth: number,
@@ -16,7 +17,11 @@ type Props = {
 type State = {
     width: number,
     height: number,
+    showClearCanvasModal: boolean,
 };
+
+const MAX_WIDTH = 32;
+const MAX_HEIGHT = 32;
 
 class CanvasControls extends React.Component<Props, State> {
     constructor(props) {
@@ -24,13 +29,14 @@ class CanvasControls extends React.Component<Props, State> {
         this.state = {
             width: this.props.initialWidth,
             height: this.props.initialHeight,
+            showClearCanvasModal: false,
         }
     }
 
     updateWidth = (event) => {
         let width = event.target.value;
-        if (width > 64) {
-            width = 64;
+        if (width > MAX_WIDTH) {
+            width = MAX_WIDTH;
         } else if (width < 1) {
             width = 1;
         }
@@ -41,8 +47,8 @@ class CanvasControls extends React.Component<Props, State> {
 
     updateHeight = (event) => {
         let height = event.target.value;
-        if (height > 64) {
-            height = 64;
+        if (height > MAX_HEIGHT) {
+            height = MAX_HEIGHT;
         } else if (height < 1) {
             height = 1;
         }
@@ -55,36 +61,68 @@ class CanvasControls extends React.Component<Props, State> {
         this.props.onResize(this.state.width, this.state.height);
     };
 
+    showClearCanvasModal = () => {
+        this.setState({
+            showClearCanvasModal: true,
+        });
+    };
+
+    onClearCanvasModalClose = () => {
+        this.setState({
+            showClearCanvasModal: false,
+        });
+    };
+
+    onClearCanvas = () => {
+        this.setState({
+            showClearCanvasModal: false,
+        });
+        this.props.onClear();
+    };
+
     render() {
         return (
-            <Container style={{ padding: '1em' }}>
-                <Row>
-                    <Form inline>
+            <React.Fragment>
+                <Container style={{ padding: '1em' }}>
+                    <Row>
+                        <Form inline>
+                            <Col>
+                                <Form.Control value={this.state.width} onChange={this.updateWidth} style={{ width: '3em' }} />
+                            </Col>
+                            <Col>
+                                x
+                            </Col>
+                            <Col>
+                                <Form.Control value={this.state.height} onChange={this.updateHeight} style={{ width: '3em' }} />
+                            </Col>
+                            <Col>
+                                <Button variant="primary" onClick={this.onResize}>
+                                    Resize
+                                </Button>
+                            </Col>
+                        </Form>
                         <Col>
-                            <Form.Control value={this.state.width} onChange={this.updateWidth} style={{ width: '3em' }} />
-                        </Col>
-                        <Col>
-                            x
-                        </Col>
-                        <Col>
-                            <Form.Control value={this.state.height} onChange={this.updateHeight} style={{ width: '3em' }} />
-                        </Col>
-                        <Col>
-                            <Button variant="primary" onClick={this.onResize}>
-                                Resize
+                            <Button
+                                variant="danger"
+                                onClick={this.showClearCanvasModal}
+                            >
+                                Clear Canvas
                             </Button>
                         </Col>
-                    </Form>
-                    <Col>
-                        <Button
-                            variant="danger"
-                            onClick={this.props.onClear}
-                        >
-                            Clear Canvas
-                        </Button>
-                    </Col>
-                </Row>
-            </Container>
+                    </Row>
+                </Container>
+                <Modal show={this.state.showClearCanvasModal} onHide={this.onClearCanvasModalClose}>
+                    <Modal.Dialog>
+                        <Modal.Body>
+                            <p>Are you sure you want to delete everything?</p>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="danger" onClick={this.onClearCanvas}>Yes</Button>
+                            <Button variant="secondary" onClick={this.onClearCanvasModalClose}>No</Button>
+                        </Modal.Footer>
+                    </Modal.Dialog>
+                </Modal>
+            </React.Fragment>
         );
     }
 }
