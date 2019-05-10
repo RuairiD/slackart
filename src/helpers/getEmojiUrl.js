31604612f297992e513247038df65275826936d4
Ruairi Dorrity <ruairidorrity@gmail.com>
2019-05-10 15:38:07 -0700

    Stock emoji support using open WebFX endpoint.

--- src/helpers/getEmojiUrl.js
+++ src/helpers/getEmojiUrl.js
@@ -1,6 +1,10 @@
 const EMOJIS = require('../config/emojis.json').emoji;
 
-const ALIAS_PREFIX = 'alias:'
+const ALIAS_PREFIX = 'alias:';
+
+const getStockEmojiUrl = (strippedEmojiName) => {
+    return `https://www.webfx.com/tools/emoji-cheat-sheet/graphics/emojis/${strippedEmojiName}.png`;
+}
 
 const getEmojiUrl = (emojiName) => {
     let strippedEmojiName = emojiName.substring(1, emojiName.length - 1);
@@ -8,7 +12,8 @@ const getEmojiUrl = (emojiName) => {
     while (!emojiUrl) {
         emojiUrl = EMOJIS[strippedEmojiName];
         if (!emojiUrl) {
-            return null;
+            // Fallback to WebFX emojis for stock emojis.
+            return getStockEmojiUrl(strippedEmojiName);
         } else if (emojiUrl.substring(0, ALIAS_PREFIX.length) === ALIAS_PREFIX) {
             strippedEmojiName = emojiUrl.substring(ALIAS_PREFIX.length, emojiUrl.length);
             emojiUrl = null;
